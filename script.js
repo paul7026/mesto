4f3695911d7e27c8bfbfb517b6204a9e85c78df2
Pavel Brudnyi <paul702600@gmail.com>
2020-07-31 20:46:30 +0700

    finish

---
+++ script.js
@@ -0,0 +1,34 @@
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile-info__edit'); // edit button
+const exitButton = document.querySelector('.popup__exit'); // exit button
+const formElement = document.querySelector('.popup__container');
+const profileName = document.querySelector('.profile-info__name') // имя в профиле
+const profileOcuppation = document.querySelector('.profile-info__occupation'); //работа в профиле
+const nameInput = document.querySelector('#popup-name'); //попап поле - имя
+const jobInput  = document.querySelector('#popup-job'); //попап поле - работа
+
+
+// ф-я открыть попап
+function openPopup() {
+  popup.classList.add('popup_opened');
+}
+
+// ф-я закрыть попап
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+// ф-я ред-я профиль
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileOcuppation.textContent = jobInput.value;
+  closePopup();
+}
+
+
+
+editButton.addEventListener('click', openPopup); 
+exitButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
