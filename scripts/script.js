e20d011a364ee28073394b8c2a04b1ba2e3d5dba
Pavel Brudnyi <paul702600@gmail.com>
2020-08-24 09:56:41 -0700

    first finish

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
 const popupAdd = document.querySelector('#popup-add'); // попап добавления
 const popupEdit = document.querySelector('#popup-edit'); // попап редактирвания
+const popupImg = document.querySelector('#popup-big-img'); // попап картинка
+const Img = popupImg.querySelector('.popup__image'); // картинка в попапе
+const ImgCaption = popupImg.querySelector('.popup__image-caption'); // подпись к картинке
 const editButton = document.querySelector('.profile-info__edit'); // edit button
 const exitButtonEdit = popupEdit.querySelector('.popup__exit'); // exit button in edit
 const exitButtonAdd = popupAdd.querySelector('.popup__exit'); // exit button in add
+const exitButtonImg = popupImg.querySelector('.popup__exit'); //exit button in Figure
 const addButton = document.querySelector('.profile__add-button'); // add button
 const formElement = document.querySelector('#popup-container-edit');
 const addFormElement =document.querySelector('#popup-container-add');
@@ -13,6 +17,10 @@ const jobInput  = document.querySelector('#popup-job'); //попап поле - 
 const elements = document.querySelector('.elements'); // выбрали секцию elements
 const placeName = document.querySelector('#place-name'); // название места
 const pictureLink = document.querySelector('#picture-link'); //ссылка на картинку
+const elementsTemplate = document.querySelector('#template-element').content;
+const place = elementsTemplate.cloneNode(true);
+
+
 
 const initialCards = [
   {
@@ -41,16 +49,27 @@ const initialCards = [
   }
 ];
 
-// ф-я отображающая набор исходных мест из initialCards
+// ф-я отображающая карточки
 initialCards.forEach(function(el) {
   const elementsTemplate = document.querySelector('#template-element').content;
   const place = elementsTemplate.cloneNode(true);
   place.querySelector('.element__title').textContent = el.name;
   place.querySelector('.element__image').src = el.link;
   place.querySelector('.element__image').alt = el.name;
+  const likeButton = place.querySelector('#element-like');
+  likeButton.addEventListener('click', like);
+  const deleteButton = place.querySelector('.element__delete-button');
+  deleteButton.addEventListener('click', deleteFunc);
+  const imageButtons = place.querySelector('.element__image');
+  console.log('imageButtons=', imageButtons);
+  imageButtons.addEventListener('click', function() {
+    popupAction(popupImg);
+    Img.src = imageButtons.src;
+    Img.alt = imageButtons.alt;
+    ImgCaption.textContent = imageButtons.alt;
+  });
   elements.append(place);
 });
-
 
 
 // ф-я ред-я профиль
@@ -75,27 +94,36 @@ function formCreateHandler (evt) {
   };
   card.name = placeName.value
   card.link = pictureLink.value
-  initialCards.push(card);
-
-
-  console.log(initialCards);
-
-  initialCards(function(el) {
-    const elementsTemplate = document.querySelector('#template-element').content;
-    const place = elementsTemplate.cloneNode(true);
-    place.querySelector('.element__title').textContent = el.name;
-    place.querySelector('.element__image').src = el.link;
-    place.querySelector('.element__image').alt = el.name;
-    // elements.append(place);
+  const elementsTemplate = document.querySelector('#template-element').content;
+  const place = elementsTemplate.cloneNode(true);
+  place.querySelector('.element__title').textContent = card.name;
+  place.querySelector('.element__image').src = card.link;
+  place.querySelector('.element__image').alt = card.name;
+  const likeButton = place.querySelector('#element-like');
+  likeButton.addEventListener('click', like);
+  const deleteButton = place.querySelector('.element__delete-button');
+  deleteButton.addEventListener('click', deleteFunc);
+  const imageButtons = place.querySelector('.element__image');
+  console.log('imageButtons=', imageButtons);
+  imageButtons.addEventListener('click', function() {
+    popupAction(popupImg);
+    Img.src = imageButtons.src;
+    Img.alt = imageButtons.alt;
+    ImgCaption.textContent = imageButtons.alt;
   });
-
-
-  // card.push(placeName.textContent);
-  // console.log(card);
-  // console.log(initialCards);
-
+  elements.prepend(place);
+  popupAction(popupAdd);
 }
 
+//ф-я поставить лайк
+  function like(evt) {
+    evt.target.classList.toggle('element__like_active');
+}
+
+//ф-я удалить карточку
+ function deleteFunc(evt) {
+    evt.target.closest('.element').remove();
+ }
 
 
 // события для попапапа редактирвания
@@ -121,6 +149,17 @@ addButton.addEventListener('click', function() {
  });
 
  addFormElement.addEventListener('submit', formCreateHandler);
+
+
+
+ exitButtonImg.addEventListener('click', function() { // закрыть попап большая картинка
+  popupAction(popupImg);
+});
+
+
+
+
+
 
 
 
